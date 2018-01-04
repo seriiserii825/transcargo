dbbfcf76e0a6a310bddec13dc48995b0875d4524
serii <seriiburduja@gmail.com>
2018-01-04 18:47:53 +0200

    + finish home animation

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -5,20 +5,39 @@ $(function(){
     $.stellar();
 
     /*
-        https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html 
-        ===========================*/    
-        $("html").easeScroll({
-          frameRate: 60,
-          animationTime: 1000,
-          stepSize: 120,
-          pulseAlgorithm: 1,
-          pulseScale: 8,
-          pulseNormalize: 1,
-          accelerationDelta: 20,
-          accelerationMax: 1,
-          keyboardSupport: true,
-          arrowScroll: 50,
-          touchpadSupport: true,
-          fixedBackground: true
-      });
+        https://www.jqueryscript.net/animation/Smooth-Mouse-Wheel-Scrolling-Plugin-With-jQuery-easeScroll.html
+        ===========================*/
+    $("html").easeScroll({
+        frameRate: 60,
+        animationTime: 1000,
+        stepSize: 90,
+        pulseAlgorithm: 1,
+        pulseScale: 8,
+        pulseNormalize: 1,
+        accelerationDelta: 20,
+        accelerationMax: 1,
+        keyboardSupport: true,
+        arrowScroll: 50,
+        touchpadSupport: true,
+        fixedBackground: true
     });
+
+    /* animate home
+    ====================================================*/
+    let wow = new WOW(
+        {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       250,          // distance to the element when triggering the animation (default is 0)
+            mobile:       true,       // trigger animations on mobile devices (default is true)
+            live:         true,       // act on asynchronously loaded content (default is true)
+            callback:     function(box) {
+                // the callback is fired every time an animation is started
+                // the argument that is passed in is the DOM node being animated
+            },
+            scrollContainer: null // optional scroll container selector, otherwise use window
+        }
+    );
+    wow.init();
+
+});
