2a155ffbfde8fc6cf52aefae0233860a39b586bb
serii <seriiburduja@gmail.com>
2018-01-11 17:08:33 +0200

    finish animation

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -28,7 +28,7 @@ $(function(){
         {
             boxClass:     'wow',      // animated element css class (default is wow)
             animateClass: 'animated', // animation css class (default is animated)
-            offset:       250,          // distance to the element when triggering the animation (default is 0)
+            offset:       150,          // distance to the element when triggering the animation (default is 0)
             mobile:       true,       // trigger animations on mobile devices (default is true)
             live:         true,       // act on asynchronously loaded content (default is true)
             callback:     function(box) {
@@ -132,7 +132,7 @@ $(function(){
     $('#js-up').on('click', function(){
         $('html, body').animate({
             scrollTop: 0
-        }, 3000);
+        }, 2000);
     });
 
     /* about-us tabs
